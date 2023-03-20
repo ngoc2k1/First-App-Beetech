a1791d1cdd76a07b353993fa2db6898a2869b6f6
ngoc2k1 <englishcoffee9999@gmail.com>
2023-03-21 00:42:08 +0700

    feat: login with facebook, google

---
+++ app/src/main/java/com/example/firstapp/MyApp.kt
@@ -0,0 +1,15 @@
+package com.example.firstapp
+
+import android.app.Application
+import com.facebook.FacebookSdk
+import com.facebook.LoggingBehavior
+import com.facebook.appevents.AppEventsLogger
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FacebookSdk.setClientToken("4913316aff7640a63ba9a8aabe01f97f")
+        FacebookSdk.sdkInitialize(applicationContext)
+        AppEventsLogger.activateApp(this)
+    }
+}
--- app/src/main/java/com/example/firstapp/featureauthen/LoginActivity.kt
+++ app/src/main/java/com/example/firstapp/featureauthen/LoginActivity.kt
@@ -2,10 +2,10 @@ package com.example.firstapp.featureauthen
 
 import android.app.ProgressDialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
-import android.text.InputType
-import android.text.InputType.*
+import android.util.Base64
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
@@ -13,6 +13,13 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firstapp.databinding.ActivityLoginBinding
 import com.example.firstapp.featurechat.ChatActivity
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.gson.Gson
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -20,12 +27,19 @@ import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import retrofit2.HttpException
 import java.io.IOException
-
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private lateinit var loadingDialog: ProgressDialog
+
+    private val callbackManager = CallbackManager.Factory.create()
+
     private var disposable: Disposable? = null
+
+    var gso: GoogleSignInOptions? = null
+    var gsc: GoogleSignInClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,20 +47,52 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setColorActionBar()
+        gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+        gsc = GoogleSignIn.getClient(this, gso!!)
 
+        try {
+            val info = packageManager.getPackageInfo(
+                "your.package",
+                PackageManager.GET_SIGNATURES
+            )
+            for (signature in info.signatures) {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.e("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (_: PackageManager.NameNotFoundException) {
+        } catch (_: NoSuchAlgorithmException) {
+        }
+
+        val loginManager = LoginManager.getInstance()
+
+        loginManager.registerCallback(callbackManager,//loginMana đăng kí vs callback để có kq tra ve
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                    Toast.makeText(this@LoginActivity, "Login success", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onCancel() {}
+
+                override fun onError(error: FacebookException) {}
+            })
+
+        binding.tvLoginFacebook.setOnClickListener(View.OnClickListener {
+            LoginManager.getInstance()
+                .logInWithReadPermissions(this@LoginActivity, listOf("public_profile"))
+        })
         binding.btLoginLogin.setOnClickListener {
             val username = binding.edtLoginUsername.text.toString()
             val password = binding.edtLoginPassword.text.toString()
             val loginRequest = LoginRequest(username, password, "asdadadsadaa", 2)
             ApiClient.chatService.createAccount("2.0.0", 2, loginRequest)
-                ?.subscribeOn(Schedulers.io())
-                ?.observeOn(AndroidSchedulers.mainThread())
+                ?.subscribeOn(Schedulers.io())?.observeOn(AndroidSchedulers.mainThread())
                 ?.subscribe(object : Observer<LoginResponse?> {
                     override fun onSubscribe(d: Disposable) {
                         disposable = d
                         loadingDialog = ProgressDialog.show(
-                            this@LoginActivity, "",
-                            "Loading. Please wait...", true
+                            this@LoginActivity, "", "Loading. Please wait...", true
                         )
                     }
 
@@ -65,14 +111,35 @@ class LoginActivity : AppCompatActivity() {
                             Toast.LENGTH_LONG
                         ).show()
                         Log.d(
-                            "error",
-                            "onError: " + throwable.getErrorBody().errorMessage.toString()
+                            "error", "onError: " + throwable.getErrorBody().errorMessage.toString()
                         )
                     }
 
                     override fun onComplete() {
                     }
                 })
+        }
+
+        binding.tvLoginGoogle.setOnClickListener {
+            val signInIntent = gsc!!.signInIntent
+            startActivityForResult(signInIntent, 1000)
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager?.let { it.onActivityResult(requestCode, resultCode, data) }//
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1000) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                task.getResult(ApiException::class.java)
+                Toast.makeText(applicationContext, "Login GG success!", Toast.LENGTH_SHORT)
+                    .show()
+            } catch (e: ApiException) {
+                Toast.makeText(applicationContext, "Something went wrong!", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
 
@@ -100,9 +167,7 @@ class LoginActivity : AppCompatActivity() {
             decorView.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             this@LoginActivity.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
-
             statusBarColor = Color.TRANSPARENT
         }
     }
-
 }
